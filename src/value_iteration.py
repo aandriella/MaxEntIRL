df11ecc56a12146203fd1975ae945aa4a948a0eb
Antonio Andriella <aandriella@iri.upc.educlear>
2020-09-23 13:54:30 +0200

    initialise policy with zeros dtype=int

--- src/value_iteration.py
+++ src/value_iteration.py
@@ -51,11 +51,11 @@ def value_iteration(P_a, rewards, gamma, error=0.01, deterministic=True):
 
 	if deterministic:
 		# generate deterministic policy
-		policy = np.zeros([N_STATES])
+		policy = np.zeros([N_STATES], dtype=int)
 		for s in range(N_STATES):
-			policy[s] = np.argmax([sum([P_a[s, s1, a] * (rewards[s] + gamma * values[s1])
+			policy[s] = (np.argmax([sum([P_a[s, s1, a] * (rewards[s] + gamma * values[s1])
 			                             for s1 in range(N_STATES)])
-			                        for a in range(N_ACTIONS)])
+			                        for a in range(N_ACTIONS)]))
 
 		return values, policy
 	else:
